4874bb956ae06727c5cfa9412acd4afddea61298
Niasha Abaglo-Grant <niashagrant11@gmail.com>
2020-06-22 21:47:30 -0700

    Updates

---
+++ precache-manifest.69917c3f73ecd1d5a5f821d933b8a44d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90af9fab30743cdc67576f30dd6b18fd",
+    "url": "/Click-Game/index.html"
+  },
+  {
+    "revision": "d68bc6e910a838e2c8ca",
+    "url": "/Click-Game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/Click-Game/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Click-Game/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d68bc6e910a838e2c8ca",
+    "url": "/Click-Game/static/js/main.815235e7.chunk.js"
+  },
+  {
+    "revision": "5a361aeab50f49728473",
+    "url": "/Click-Game/static/js/runtime-main.8a0c750c.js"
+  },
+  {
+    "revision": "113945106775b0c5930d06cee62dd0f8",
+    "url": "/Click-Game/static/media/logo.11394510.svg"
+  }
+]);
