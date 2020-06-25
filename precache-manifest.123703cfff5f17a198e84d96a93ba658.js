2bd338a950aff9e186fd73616860a04e93820ee0
Niasha Abaglo-Grant <niashagrant11@gmail.com>
2020-06-24 21:34:00 -0700

    Updates

---
+++ precache-manifest.123703cfff5f17a198e84d96a93ba658.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d658b2a7ff1f761ddc7f1f4c31707b4",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "0302ccd285bc5ee2ec92",
+    "url": "/Clicky-Game/static/css/main.0042cb9d.chunk.css"
+  },
+  {
+    "revision": "54322a8d214aa958bcfd",
+    "url": "/Clicky-Game/static/js/2.6bdb3ffa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Game/static/js/2.6bdb3ffa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0302ccd285bc5ee2ec92",
+    "url": "/Clicky-Game/static/js/main.7be2b7ef.chunk.js"
+  },
+  {
+    "revision": "79776d704002601449fa",
+    "url": "/Clicky-Game/static/js/runtime-main.758e6d5e.js"
+  }
+]);
